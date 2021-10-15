029cccd4b7585df9b772412d59fa5d8506981c6a
Kim Seongje <bulkybobby92@gmail.com>
2021-10-15 14:47:44 +0900

    자바스크립트 주석 추가

--- js/main.js
+++ js/main.js
@@ -1,21 +1,26 @@
 
-// 스크롤 애니메이션
+// 스크롤 애니메이션 //
 AOS.init();
+// 스크롤 애니메이션 //
 
-// 주소 입력 pop-up 팝업 이상 없음
+// 주소 입력 pop-up //
 function fn_openAddressPopup() {
     var url = "addersspopup.html";
     var name = "AddressPopup";
     var option = "width=650, height=500, top=100, left=200, location=no"
     window.open(url, name, option);
 }
+// 주소 입력 pop-up //
 
+// 검색주소 불러오기 //
 function callback_openAddressPopup(aParam) {
     document.getElementById("basic-address").value = aParam["roadAddr"];
 }
+// 검색주소 불러오기 //
 
-// 주소 입력 불러오기 이상 없음
-// 주소명으로 검색
+/// 주소 검색 API ///
+
+// 주소명으로 검색 //
 function fn_search() {
 
     $.ajax({
@@ -41,7 +46,9 @@ function fn_search() {
     });
 
 }
-//  결과 테이블 생성
+// 주소명으로 검색 //  
+
+// 결과 테이블 생성 //
 function fn_makeListJson(jsonStr) {
     var htmlStr = "";
     $(jsonStr.results.juso).each(
@@ -59,7 +66,9 @@ function fn_makeListJson(jsonStr) {
     $("#addressTableTbody").html(htmlStr);
 
 }
-//  Enter 키 이벤트
+// 결과 테이블 생성 //
+
+// Enter 키 이벤트 //
 function enterSearch() {
     var evt_code = (window.netscape) ? ev.which : event.keyCode;
     if (evt_code == 13) {
@@ -67,7 +76,9 @@ function enterSearch() {
         fn_search(); //jsonp사용시 enter검색 
     }
 }
-//  주소 선택
+// Enter 키 이벤트 //
+
+// 주소 선택 //
 function chooseAddress(roadAddr, jibunAddr, zipNo) {
     var aParam = [];
     aParam["roadAddr"] = roadAddr;
@@ -77,11 +88,17 @@ function chooseAddress(roadAddr, jibunAddr, zipNo) {
     opener.callback_openAddressPopup(aParam);
     window.close();
 }
+// 주소 선택 //
 
-//로그인 팝업
+/// 주소 검색 API ///
+
+
+/// 로그인 팝업 ///
 function fn_openLoginPopup() {
     var url = "loginpopup.html";
     var name = "loginPopup";
     var option = "width=650, height=500, top=100, left=200, location=no"
     window.open(url, name, option);
 }
+/// 로그인 팝업 ///
+
