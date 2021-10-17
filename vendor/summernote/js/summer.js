3c02537448032c01bf634b931a7cd6a8b9dc8dc5
Kim Seongje <bulkybobby92@gmail.com>
2021-10-17 12:46:05 +0900

    게시글 작성 크기, 인풋 태그 수정

--- vendor/summernote/js/summer.js
+++ vendor/summernote/js/summer.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	{ var fontList = ['맑은 고딕','NotoSansKR','맑은 고딕','궁서','굴림체','굴림','돋움체','바탕체']; 
 	$('#summernote').summernote({ 
 		height: 300,
-		width: 700,
+		width: 1060,
 		lang: 'ko-KR', 
 		fontNames: fontList, 
 		fontNamesIgnoreCheck: fontList,
