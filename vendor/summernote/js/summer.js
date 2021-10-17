33cab2dd54036d8b548a4577a684b69366b2a718
Kim Seongje <bulkybobby92@gmail.com>
2021-10-18 03:14:04 +0900

    게시글 작성 수정

--- vendor/summernote/js/summer.js
+++ vendor/summernote/js/summer.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 	$(document).ready(function() 
 	{ var fontList = ['맑은 고딕','NotoSansKR','맑은 고딕','궁서','굴림체','굴림','돋움체','바탕체']; 
 	$('#summernote').summernote({ 
-		height: 300,
+		height: 400,
 		width: 1060,
 		lang: 'ko-KR', 
 		fontNames: fontList, 
