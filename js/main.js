82fa3c064a22453aa628d79f5e259c86240ae92e
sts1144 <tlsxotnsla@gmail.com>
2021-10-18 17:47:35 +0900

    게시판 [배너 애니메이션 추가] 완료

--- js/main.js
+++ js/main.js
@@ -124,3 +124,22 @@ const closeModal = () => {
 clsoeButton.addEventListener("click", closeModal);
 openButton.addEventListener("click", openModal);
 /// 로그인 모달 ///
+
+
+//플로팅 배너//
+var floatPosition = parseInt($(".sideBanner").css('top'))
+
+// scroll 인식
+$(window).scroll(function () {
+
+    // 현재 스크롤 위치
+    var currentTop = $(window).scrollTop();
+    var bannerTop = currentTop + floatPosition + "px";
+
+    //이동 애니메이션
+    $(".sideBanner").stop().animate({
+        "top": bannerTop
+    }, 500);
+
+}).scroll();
+
